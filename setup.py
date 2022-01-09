18452a4e521aafe4f4e1c66940d86a90f71d801d
Daniel Steman <danielsteman@daniels-mbp13.home>
2022-01-09 18:57:03 +0100

    added dependencies to setup

--- setup.py
+++ setup.py
@@ -3,14 +3,17 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+install_requires = ["python-dotenv==0.19.2", "requests==2.26.0"]
+
 setuptools.setup(
     name="depyro",
-    version="0.0.1",
+    version="1.0.0",
     author="Daniel Steman",
     author_email="daniel-steman@live.nl",
     description="A wrapper for the De Giro API to fetch portfolio data",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    install_requires=install_requires,
     url="https://github.com/danielsteman/depyro",
     project_urls={"Bug Tracker": "https://github.com/danielsteman/depyro/issues"},
     classifiers=[
